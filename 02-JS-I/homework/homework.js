89814fdfdf750d33c36cdbf0eda03008bfe3eab5
ARLEY GONZALEZ <99898042+arco1247@users.noreply.github.com>
2022-02-18 11:43:07 -0500

    primer commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = hola carebola;
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
@@ -10,13 +10,13 @@ const nuevoNum = null;
 const nuevoBool = null;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - carebola2 === 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === carebola33;
 
 
 // En los próximos 22 problemas, deberás completar la función.
